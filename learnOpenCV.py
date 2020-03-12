2154c1a267656b06efa74e16efd964cfe8eca4ee
Liberhome <46431509+Liberhome@users.noreply.github.com>
2020-03-12 09:52:43 +0800

    Add files via upload

---
+++ learnOpenCV.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import matplotlib as plt
+import numpy as np
+
+
+img=cv.imread("catlina.png")
+#图像显示
+cv.startWindowThread() #加在这个位置
+cv.imshow('image',img)
+cv.waitKey(0)
+
+img2=cv.imread("catlina.png",cv.IMREAD_GRAYSCALE) #灰度图
+cv.imshow('image',img2)
+cv.waitKey(0)
+
